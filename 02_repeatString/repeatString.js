7e2ffd3215e6074302099216dcc9c5afcdda8a6e
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-16 10:35:25 -0500

    Finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let longer_string = '';
+    for(let i = 0; i < num; i++){
+        longer_string += string;
+    }
+    return longer_string;
 };
 
 // Do not edit below this line
