c14ad3eb7217469934062b768f8ec0c6a0292219
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-27 14:51:10 -0500

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest;
+    let oldest_age = 0;
+    for (let i = 0; i < people.length; i++) {
+        let person_age;
+        if ("yearOfDeath" in people[i] === false) {
+            let currentYear = new Date().getFullYear();
+            person_age = currentYear - people[i].yearOfBirth;
+        }
+        else {
+            person_age = people[i].yearOfDeath - people[i].yearOfBirth;
+            console.log(person_age);
+        }
 
+        if(person_age > oldest_age){
+            oldest_age = person_age;
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
